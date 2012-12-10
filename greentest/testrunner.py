a8ac7937463e8349902d333e586406399d528a1c
Fantix King <fantix.king@gmail.com>
2012-12-10 11:09:18 +0800

    Refs #38, fixed __nonzero__ and dict.items().

--- greentest/testrunner.py
+++ greentest/testrunner.py
@@ -69,7 +69,7 @@ def run_many(tests, expected=None, failfast=False):
 
     if NWORKERS > 1 and toretry:
         util.log('\nWill retry %s failed tests sequentially:\n- %s\n', len(toretry), '\n- '.join(toretry))
-        for name, (cmd, kwargs, _ignore) in failed.items():
+        for name, (cmd, kwargs, _ignore) in [x for x in failed.items()]:
             if not util.run(cmd, buffer_output=False, **kwargs):
                 failed.pop(name)
                 failed_then_succeeded.append(name)
